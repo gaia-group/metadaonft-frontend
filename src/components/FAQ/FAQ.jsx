c6d5f939f16aebdb57b170fffc008eb050255ded
archit3c <boomerclub@protonmail.com>
2022-02-02 13:21:03 -0700

    Updates FAQ

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -11,74 +11,24 @@ const faqs = [
     answer: (
       <div className="space-y-3">
         <p>
-          You&apos;ll be part of a community of likeminded people who want to
-          build in this world.
+          First and most important - you will be minting uniquely stunning
+          metaverse land.
         </p>
 
         <p>
-          Meta DAO pools funds from thousands of people and then buys real
-          estate inside the Metaverse.
+          Next, you get access to the first metaverse focused community. Think
+          of it like being in Manhattan in 1880 and building a network... but in
+          the digital world. You&apos;ll be part of an exclusive club of
+          likeminded people who want to innovate in this world.
         </p>
 
-        <p>More specifically, as a Meta DAO NFT Holder: </p>
-
-        <ol className="space-y-3 list-decimal list-outside ml-7">
-          <li>
-            You get 1 vote in the DAO per NFT you own when there is a decision
-            open to voting.
-          </li>
-
-          <li>
-            Your NFT will allow you to claim an ERC-20 token ($GAIA) after the
-            initial land purchases are made. $GAIA represents fractionalized
-            ownership of the purchased land. You can hold $GAIA to retain
-            ownership of the land, or sell it in the open market.
-          </li>
-
-          <li className="space-y-3">
-            You can claim pre-sale whitelisting on all future NFT mints. Gaia
-            Group plans to fund numerous land purchases with similar NFT drops
-            to Meta DAO. The roadmap currently includes 3 drops within the next
-            12 months.{' '}
-            <p>
-              Just holding your NFT means you naturally gain greater exposure to
-              pristine Metaverse real estate.{' '}
-            </p>
-            <p>For example: </p>
-            <ul className="space-y-3 list-disc list-outside ml-7">
-              <li>Meta DAO secondary sales = 100 ETH </li>
-              <li>
-                Secondary sales royalty earned - 100 ETH * 7% Royalty = 7 ETH{' '}
-              </li>
-              <li>DAO profit earned - 7 ETH * 75% = 5.25 ETH </li>
-              <li>
-                Meta DAO takes that 5.25 ETH and buys more Metaverse land. This
-                means your fractionalized ownership of your $GAIA token is now
-                backed by more land than before.
-              </li>
-            </ul>
-          </li>
-
-          <li>
-            You have the ability to stake your NFT(s) in order to earn more of
-            the $GAIA token. This further increases your position of
-            fractionalized ownership inside the DAO and thus Metaverse land
-            passively.
-          </li>
-
-          <li>
-            Get an awesome piece of artwork you can display digitally or in the
-            real world.
-          </li>
-        </ol>
+        <p>
+          Lastly, you get early access to purchase other products launched by
+          either Gaia Group of Meta DAO, including our second product that
+          brings your digital collectible to life.
+        </p>
       </div>
     ),
-  },
-  {
-    id: count++,
-    question: 'How much of the funds go into the DAO?',
-    answer:
-      '75% of both the initial mint and secondary sales will be going into the DAO.',
   },
   {
     id: count++,
@@ -88,31 +38,25 @@ const faqs = [
   {
     id: count++,
     question: 'How many Meta DAO NFTs can we mint?',
-    answer: '1 per wallet',
+    answer:
+      '1 per wallet during the whitelist sale. 2 per wallet during the public sale.',
   },
   {
     id: count++,
     question: 'How many Meta DAO NFTs will be available to mint?',
-    answer:
-      'The Meta DAO collection comprises of 10,000 total NFTs (3000 genesis to start). We may never release all 10,000 NFTs if we actualize our property purchasing goals with less mints. We believe this approach grows a high quality, aligned community.',
+    answer: 'The Meta DAO collection comprises of 10,000 total land NFTs.',
   },
   {
     id: count++,
     question: 'How do I mint or buy a Meta DAO NFT?',
     answer:
-      'We will provide exact instructions on how to mint in our announcements channel on Discord. The mint will take place only on this website. Do not ever click on links or talk to people in DMs. They are all 100% a scam. Always follow the instructions inside the announcements channel.',
+      'We will provide exact instructions on how to mint in our discord. The mint will take place only on this official website. Do not ever click on links or talk to people in DMs. They are all 100% a scam. Always follow the instructions in our discord or on this site.',
   },
   {
     id: count++,
     question: 'What is Gaia Group?',
     answer:
-      'Gaia Group is the overarching brand that is launching Meta DAO and future NFT projects. Gaia in greek methodology is the goddess of the earth. It was fitting to use her name as we take over the prime pieces of land through all Metaverses.',
-  },
-  {
-    id: count++,
-    question: 'Will $GAIA token be used for all NFTs created in the future?',
-    answer:
-      'Yes, $GAIA will be the only token used for all NFTs projects released by the Gaia Group.',
+      'Gaia Group is the overarching brand that is launching Meta DAO and future NFT projects. Gaia is the goddess of the earth and we use her name as we produce more land projects in the metaverse.',
   },
   {
     id: count++,
@@ -125,20 +69,9 @@ const faqs = [
           and the communities created around them.
         </p>
         <p>
-          The Meta DAO focuses on its own Metaverse land acquisition strategy.
-          The other NFT projects we release will have their own focus and
-          acquisition strategies.
-        </p>
-        <p>For example:</p>
-        <ul className="ml-2 list-disc list-inside">
-          <li>Meta DAO -&gt; DAO votes to buy land in The Sandbox </li>
-          <li>Mint Project #2 -&gt; DAO votes to buy land in Decentraland </li>
-          <li>Mint Project #3 -&gt; DAO votes to buy land in Netvrk</li>
-        </ul>
-        <p>
-          Gaia Group gives every consequent NFT project significantly more
-          strength, since its perpetually backed by more Metaverse land and
-          successful project launches.
+          The Meta DAO focuses on making it easier to own and build onq
+          metaverse land. The other NFT projects we release will have their own
+          focus.
         </p>
       </div>
     ),
