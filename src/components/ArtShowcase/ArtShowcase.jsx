0a76c334d17c4d789aa21ac1c01574e36d21fdac
archit3c <boomerclub@protonmail.com>
2022-02-02 17:41:23 -0700

    Updates art showcase for visibility.

--- src/components/ArtShowcase/ArtShowcase.jsx
+++ src/components/ArtShowcase/ArtShowcase.jsx
@@ -12,7 +12,7 @@ import preview9 from '../../images/preview-9.png'
 import preview10 from '../../images/preview-10.png'
 
 let count = 0
-const pieces = [
+const categories = [
   {
     id: count++,
     imageSrc: preview1,
@@ -57,23 +57,28 @@ const pieces = [
 
 function ArtShowcase() {
   return (
-    <div className="bg-black py-2">
-      <div className="xl:max-w-7xl xl:mx-auto xl:px-8">
+    <div className="bg-black py-6">
+      <div className="xl:mx-auto">
         <div className="flow-root">
           <div className="-my-2">
-            <div className="box-content relative py-2 overflow-x-auto h-80 xl:overflow-visible">
-              <div className="absolute flex px-2 space-x-8 min-w-screen-xl sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-5 xl:gap-x-8">
-                {pieces.map((piece) => (
+            <div className="box-content relative py-2 overflow-x-auto h-80">
+              <div className="absolute flex px-2 space-x-4 min-w-screen-xl sm:px-4 lg:px-6">
+                {categories.map((category) => (
                   <div
-                    key={piece.imgSrc}
-                    className="relative flex flex-col w-80 p-6 overflow-hidden rounded-lg h-80 xl:w-auto"
+                    key={category.imgSrc}
+                    className="relative group flex flex-col w-80 overflow-hidden rounded-lg h-80"
                   >
                     <span aria-hidden="true" className="absolute inset-0">
                       <img
-                        src={piece.imageSrc}
+                        src={category.imageSrc}
+                        alt=""
                         className="object-cover object-center w-full h-full"
                       />
                     </span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-x-0 bottom-0 transition-opacity duration-200 opacity-0 group-hover:opacity-50 h-2/3 bg-gradient-to-t from-gray-800"
+                    />
                   </div>
                 ))}
               </div>
