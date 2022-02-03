43cd90ad5c91e3ee44fe6a751fc389098aff1eae
archit3c <boomerclub@protonmail.com>
2022-02-02 17:21:52 -0700

    Compress art and showcase it better.

--- src/components/ArtShowcase/ArtShowcase.jsx
+++ src/components/ArtShowcase/ArtShowcase.jsx
@@ -12,7 +12,7 @@ import preview9 from '../../images/preview-9.png'
 import preview10 from '../../images/preview-10.png'
 
 let count = 0
-const categories = [
+const pieces = [
   {
     id: count++,
     imageSrc: preview1,
@@ -57,28 +57,23 @@ const categories = [
 
 function ArtShowcase() {
   return (
-    <div className="bg-black py-6">
+    <div className="bg-black py-2">
       <div className="xl:max-w-7xl xl:mx-auto xl:px-8">
         <div className="flow-root">
           <div className="-my-2">
-            <div className="box-content relative `p`y-2 overflow-x-auto h-80 xl:overflow-visible">
-              <div className="absolute flex px-4 space-x-8 min-w-screen-xl sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-5 xl:gap-x-8">
-                {categories.map((category) => (
+            <div className="box-content relative py-2 overflow-x-auto h-80 xl:overflow-visible">
+              <div className="absolute flex px-2 space-x-8 min-w-screen-xl sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-5 xl:gap-x-8">
+                {pieces.map((piece) => (
                   <div
-                    key={category.imgSrc}
-                    className="relative flex flex-col w-56 p-6 overflow-hidden rounded-lg h-80 xl:w-auto"
+                    key={piece.imgSrc}
+                    className="relative flex flex-col w-80 p-6 overflow-hidden rounded-lg h-80 xl:w-auto"
                   >
                     <span aria-hidden="true" className="absolute inset-0">
                       <img
-                        src={category.imageSrc}
-                        alt=""
+                        src={piece.imageSrc}
                         className="object-cover object-center w-full h-full"
                       />
                     </span>
-                    <span
-                      aria-hidden="true"
-                      className="absolute inset-x-0 bottom-0 opacity-50 h-2/3 bg-gradient-to-t from-gray-800"
-                    />
                   </div>
                 ))}
               </div>
