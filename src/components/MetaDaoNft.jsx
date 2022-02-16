c8f5fa0000efe2aaca767b2073984bbb53081778
archit3c <boomerclub@protonmail.com>
2022-02-16 15:10:34 -0700

    Adds art next to roadmap.

--- src/components/MetaDaoNft.jsx
+++ src/components/MetaDaoNft.jsx
@@ -19,6 +19,7 @@ import twitterSocialLogo from '../images/twitter-social-logo.svg'
 import Header from './Header/Header'
 import ArtShowcase from './ArtShowcase/ArtShowcase'
 import ArtistSpotlight from './ArtistSpotlight/ArtistSpotlight'
+import Roadmap from './Roadmap/Roadmap'
 import FAQ from './FAQ/FAQ'
 import { getWhitelistParams } from '../utilities/merkleTrees'
 
@@ -164,54 +165,7 @@ export default function MetaDaoNft() {
         />
         <ArtShowcase />
         <ArtistSpotlight />
-        <div className="flex flex-col items-center justify-center mx-auto bg-gray-900 md:flex-row">
-          <div className="order-last w-full max-w-3xl px-4 py-12 border-gray-700 md:order-first sm:px-6 lg:px-8 lg:py-16">
-            <h2 className="py-8 mt-2 text-3xl font-extrabold tracking-tight text-left text-gray-200 sm:text-4xl">
-              The Roadmap
-            </h2>
-            <p className="text-lg text-left text-gray-200 sm:text-2xl">
-              <span className="font-extrabold text-green-300">First</span>
-              {', '}
-              sell 4,444 NFT collectibles.
-            </p>
-            <p className="text-lg text-left text-gray-200 sm:text-2xl">
-              <span className="font-extrabold text-green-300">Next</span>
-              {', '}
-              bring your collectible to life.
-            </p>
-            <div className="space-y-2 sm:space-y-4 pt-6 sm:pt-10">
-              <p className="text-gray-200 sm:text-lg text-base">
-                Giving people tools to own and access digital goods and property
-                is crucial in this next phase of the internet.
-              </p>
-
-              <p className="text-gray-200 sm:text-lg text-base">
-                Our first product is a collectible artwork that you can treasure
-                on the blockchain forever. This collectible may be the first
-                piece of digital property that you truly own.
-              </p>
-
-              <p className="text-gray-200 sm:text-lg text-base">
-                This collectible represents a unique plot of land in an
-                impossible universe. A universe waiting to be created and
-                explored.
-              </p>
-
-              <p className="text-gray-200 sm:text-lg text-base">
-                It also gives you access to future product launches – products
-                that will enable you to build your own unique experiences
-                easily, and that bridge the gap between your physical and
-                digital spaces.{' '}
-              </p>
-
-              <p className="text-gray-200 sm:text-lg text-base">
-                Our next product will work with our first collectible to achieve
-                this mission, and will be unveiled in Q2 of 2022.
-              </p>
-            </div>
-          </div>
-        </div>
-
+        <Roadmap />
         <LinksSection />
         <FAQ />
         <footer className="bg-black border-t border-gray-800">
--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -1,189 +1,103 @@
 import React from 'react'
-import { CheckIcon } from '@heroicons/react/solid'
-import classNames from 'classnames'
 
-const steps = [
-  {
-    name: 'Q1 2022',
-    description: (
-      <ul className="py-1 ml-1 space-y-3">
-        <li>Launch the Gen-0 NFT to fund land purchases</li>
-        <li>
-          <a
-            className="text-green-300 hover:opacity-80"
-            href="https://snapshot.org/"
-          >
-            Snapshot
-          </a>{' '}
-          vote on Gen-0 land purchase
-        </li>
-        <li>Purchase Gen-0 land</li>
-      </ul>
-    ),
-    status: 'current',
-  },
-  {
-    name: 'Q2 2022',
-    description: (
-      <ul className="py-1 ml-1 space-y-3">
-        <li>
-          Develop <span className="font-semibold">$GAIA</span>, fractionalizing
-          land ownership
-        </li>
-        <li>
-          Distribute{' '}
-          <span className="font-semibold">1M $GAIA per ETH in the DAO</span> to
-          all Gen-0 holders.
-        </li>
-      </ul>
-    ),
-    status: 'upcoming',
-  },
-  {
-    name: 'Q3 2022',
-    description: (
-      <ul className="py-1 ml-1 space-y-3">
-        <li>Launch Gen-1 NFT to fund next purchase</li>
-        <li>
-          <a
-            className="text-green-300 hover:opacity-80"
-            href="https://snapshot.org/"
-          >
-            Snapshot
-          </a>{' '}
-          vote on Gen-1 land purchase
-        </li>
-        <li>Purchase Gen-1 land</li>
-        <li>
-          Distribute{' '}
-          <span className="font-semibold">900k $GAIA per ETH in the DAO</span>{' '}
-          to all Gen-1 holders.
-        </li>
-      </ul>
-    ),
-    status: 'upcoming',
-  },
-  {
-    name: 'Q4 2022',
-    description: (
-      <ul className="py-1 ml-1 space-y-3">
-        <li>Launch Gen-2 NFT to fund next purchase</li>
-        <li>
-          <a
-            className="text-green-300 hover:opacity-80"
-            href="https://snapshot.org/"
-          >
-            Snapshot
-          </a>{' '}
-          vote on Gen-2 land purchase
-        </li>
-        <li>Purchase Gen-2 land</li>
-        <li>
-          Distribute{' '}
-          <span className="font-semibold">800k $GAIA per ETH in the DAO</span>{' '}
-          to all Gen-2 holders.
-        </li>
-      </ul>
-    ),
-    status: 'upcoming',
-  },
-]
+import preview from '../../images/preview-11.png'
 
 function Roadmap() {
   return (
-    <nav aria-label="Progress" className="flex justify-center bg-gray-900">
-      <ol role="list" className="overflow-hidden">
-        {steps.map((step, stepIdx) => (
-          <li
-            key={step.name}
-            className={classNames(
-              stepIdx !== steps.length - 1 ? 'pb-10' : '',
-              'relative'
-            )}
-          >
-            {step.status === 'complete' ? (
-              <>
-                {stepIdx !== steps.length - 1 ? (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-green-300"
-                    aria-hidden="true"
-                  />
-                ) : null}
-                <div className="relative flex items-start group">
-                  <span className="flex items-center h-9">
-                    <span className="relative z-10 flex items-center justify-center w-8 h-8 bg-green-300 rounded-full group-hover:bg-green-800">
-                      <CheckIcon
-                        className="w-5 h-5 text-white"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </span>
-                  <span className="flex flex-col min-w-0 pb-1 ml-4 ">
-                    <span className="text-2xl font-semibold tracking-wide text-green-300 uppercase md:text-3xl">
-                      {step.name}
-                    </span>
-                    <span className="text-base text-gray-300 md:text-lg">
-                      {step.description}
-                    </span>
-                  </span>
-                </div>
-              </>
-            ) : step.status === 'current' ? (
-              <>
-                {stepIdx !== steps.length - 1 && (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-600"
-                    aria-hidden="true"
-                  />
-                )}
-                <div
-                  className="relative flex items-start group"
-                  aria-current="step"
+    <div className="py-16 bg-gray-900 overflow-hidden lg:py-24">
+      <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+        <div className="relative lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+            <svg
+              className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+              width={784}
+              height={404}
+              fill="none"
+              viewBox="0 0 784 404"
+            >
+              <defs>
+                <pattern
+                  id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
                 >
-                  <span className="flex items-center h-9" aria-hidden="true">
-                    <span className="relative z-10 flex items-center justify-center w-8 h-8 bg-gray-900 border-2 border-green-300 rounded-full">
-                      <span className="h-2.5 w-2.5 bg-green-700 rounded-full" />
-                    </span>
-                  </span>
-                  <span className="flex flex-col min-w-0 ml-4">
-                    <span className="pb-1 text-2xl font-semibold tracking-wide text-gray-300 uppercase md:text-3xl">
-                      {step.name}
-                    </span>
-                    <span className="text-base text-gray-300 md:text-lg">
-                      {step.description}
-                    </span>
-                  </span>
-                </div>
-              </>
-            ) : (
-              <>
-                {stepIdx !== steps.length - 1 ? (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-600"
-                    aria-hidden="true"
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-800"
+                    fill="currentColor"
                   />
-                ) : null}
-                <div className="relative flex items-start group">
-                  <span className="flex items-center h-9" aria-hidden="true">
-                    <span className="relative z-10 flex items-center justify-center w-8 h-8 bg-gray-900 border-2 border-gray-600 rounded-full">
-                      <span className="h-2.5 w-2.5 bg-transparent rounded-full" />
-                    </span>
-                  </span>
-                  <span className="flex flex-col min-w-0 ml-4">
-                    <span className="pb-1 text-2xl font-semibold tracking-wide text-gray-300 uppercase md:text-3xl">
-                      {step.name}
-                    </span>
-                    <span className="text-base text-gray-300 md:text-lg">
-                      {step.description}
-                    </span>
-                  </span>
-                </div>
-              </>
-            )}
-          </li>
-        ))}
-      </ol>
-    </nav>
+                </pattern>
+              </defs>
+              <rect
+                width={784}
+                height={404}
+                fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
+              />
+            </svg>
+            <img
+              className="relative mx-auto"
+              width={490}
+              src={preview}
+              alt="Art Preview"
+            />
+          </div>
+
+          <div className="flex flex-col items-center justify-center mx-auto bg-gray-900 md:flex-row">
+            <div className="order-last w-full max-w-3xl px-4 border-gray-700 md:order-first sm:px-6 lg:px-8">
+              <h2 className="py-8 mt-2 text-3xl font-extrabold tracking-tight text-left text-gray-200 sm:text-4xl">
+                The Roadmap
+              </h2>
+              <p className="text-lg text-left text-gray-200 sm:text-2xl">
+                <span className="font-extrabold text-green-300">First</span>
+                {', '}
+                sell 4,444 NFT collectibles.
+              </p>
+              <p className="text-lg text-left text-gray-200 sm:text-2xl">
+                <span className="font-extrabold text-green-300">Next</span>
+                {', '}
+                bring your collectible to life.
+              </p>
+              <div className="space-y-2 sm:space-y-4 pt-6 sm:pt-10">
+                <p className="text-gray-200 sm:text-lg text-base">
+                  Giving people tools to own and access digital goods and
+                  property is crucial in this next phase of the internet.
+                </p>
+
+                <p className="text-gray-200 sm:text-lg text-base">
+                  Our first product is a collectible artwork that you can
+                  treasure on the blockchain forever. This collectible may be
+                  the first piece of digital property that you truly own.
+                </p>
+
+                <p className="text-gray-200 sm:text-lg text-base">
+                  This collectible represents a unique plot of land in an
+                  impossible universe. A universe waiting to be created and
+                  explored.
+                </p>
+
+                <p className="text-gray-200 sm:text-lg text-base">
+                  It also gives you access to future product launches – products
+                  that will enable you to build your own unique experiences
+                  easily, and that bridge the gap between your physical and
+                  digital spaces.{' '}
+                </p>
+
+                <p className="text-gray-200 sm:text-lg text-base">
+                  Our next product will work with our first collectible to
+                  achieve this mission, and will be unveiled in Q2 of 2022.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
