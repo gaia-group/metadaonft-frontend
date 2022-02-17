fd6a114c8fc20479dd661062f262d78a1f747412
archit3c <boomerclub@protonmail.com>
2022-02-17 16:48:17 -0700

    Always show minting tracker.

--- src/components/PublicCountdown/PublicCountdown.jsx
+++ src/components/PublicCountdown/PublicCountdown.jsx
@@ -5,15 +5,15 @@ import { PUBLIC_LAUNCH_DATETIME } from '../Countdown/Countdown'
 
 function PublicCountdown({ lastRecordedTime, isWrongNetwork, totalSupply }) {
   return (
-    <ReactCountdown
-      date={PUBLIC_LAUNCH_DATETIME.valueOf()}
-      now={() => lastRecordedTime}
-      renderer={({ hours, minutes, seconds, completed }) => {
-        if (!completed) {
-          return (
-            <div className="text-xl text-white font-extrabold tracking-tight text-center sm:text-2xl lg:text-3xl text-stroke-black pt-10 space-y-2">
-              <p>Pre-sale is open</p>
-              <div>
+    <div className="text-xl text-white font-extrabold tracking-tight text-center sm:text-2xl lg:text-3xl text-stroke-black pt-10 space-y-2">
+      <ReactCountdown
+        date={PUBLIC_LAUNCH_DATETIME.valueOf()}
+        now={() => lastRecordedTime}
+        renderer={({ hours, minutes, seconds, completed }) => {
+          if (!completed) {
+            return (
+              <>
+                <p>Pre-sale is open</p>
                 <p>
                   Public starts in
                   <span className="tracking-wider text-green-300 font-mono ml-2">
@@ -22,23 +22,19 @@ function PublicCountdown({ lastRecordedTime, isWrongNetwork, totalSupply }) {
                     {String(seconds).padStart(2, '0')}
                   </span>
                 </p>
-              </div>
-            </div>
-          )
-        } else {
-          return (
-            <div className="text-xl text-white font-extrabold tracking-tight text-center sm:text-2xl lg:text-3xl text-stroke-black pt-10 space-y-2">
-              <p>Public minting is open</p>
-              <p className="tracking-wider text-green-300">
-                {isWrongNetwork
-                  ? '⚠️ Must connect to mainnet'
-                  : `${totalSupply}/4444 minted`}
-              </p>
-            </div>
-          )
-        }
-      }}
-    />
+              </>
+            )
+          } else {
+            return <p>Public minting is open</p>
+          }
+        }}
+      />
+      <p className="tracking-wider text-green-300">
+        {isWrongNetwork
+          ? '⚠️ Must connect to mainnet'
+          : `${totalSupply}/4444 minted`}
+      </p>
+    </div>
   )
 }
 
