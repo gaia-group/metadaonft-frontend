ddaecd3b7e052b5522761ee0f1a8ed9f8b7ddb8d
archit3c <boomerclub@protonmail.com>
2022-02-17 16:46:00 -0700

    Adds question about wen reveal

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -49,7 +49,7 @@ function Roadmap() {
           </div>
 
           <div className="flex flex-col items-center justify-center mx-auto bg-gray-900 md:flex-row">
-            <div className="order-last w-full max-w-3xl px-4 border-gray-700 md:order-first sm:px-6 lg:px-8">
+            <div className="order-last w-full max-w-3xl px-4 border-gray-700 md:order-first sm:px-6 lg:px-8 z-10">
               <h2 className="py-8 mt-2 text-3xl font-extrabold tracking-tight text-left text-gray-200 sm:text-4xl">
                 The Roadmap
               </h2>
